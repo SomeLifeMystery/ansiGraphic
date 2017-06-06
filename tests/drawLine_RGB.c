9c4459940763a6f432b7e7ef6294507ab993162c
Hadrien Daures <hadrien.daures@gmail.com>
2017-06-06 11:38:38 +0200

    RGB and not RGB examples

--- tests/drawLine_RGB.c
+++ tests/drawLine_RGB.c
@@ -6,7 +6,9 @@
 int main() {
   const int width = 60;
   const int height = 30;
-  ansigraphic_image_t*image=ansigraphic_newImage(width, height);
+  ansigraphic_color_RGB_t white;
+  ansigraphic_color_RGB_set(&white, 255, 255, 255);
+  ansigraphic_image_RGB_t*image=ansigraphic_newImage_RGB(width, height);
   ansigraphic_dvector2_t start, end;
   double alpha = 0;
   double beta = 0;
@@ -18,14 +20,14 @@ int main() {
   start.x=width/2;
   start.y=height/2;
   while (alpha < 3600) {
-    ansigraphic_imageClear(image);
+    ansigraphic_imageClear_RGB(image);
     end.x=start.x + radiusx*(cos(((int)(alpha)%360)*3.14/180.0));
     end.y=start.y + radiusy*(sin(((int)(alpha)%360)*3.14/180.0));
-    ansigraphic_drawLine(image, &start, &end, '.', "015", "000");
+    ansigraphic_drawLine_RGB(image, &start, &end, ' ', &white, &white);
     end.x=start.x + radiusbx*(cos(((int)(beta)%360)*3.14/180.0));
     end.y=start.y + radiusby*(sin(((int)(beta)%360)*3.14/180.0));
-    ansigraphic_drawLine(image, &start, &end, '.', "015", "000");
-    ansigraphic_imagePrint(image);
+    ansigraphic_drawLine_RGB(image, &start, &end, ' ', &white, &white);
+    ansigraphic_imagePrint_RGB(image);
     alpha+=1;
     beta+=3;
   }
