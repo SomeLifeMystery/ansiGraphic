a242dd74df1882113327d890948706071121bdb8
Hadrien Daures <hadrien.daures@gmail.com>
2017-07-25 13:53:21 +0200

    slower display

--- examples/bmp.c
+++ examples/bmp.c
@@ -7,10 +7,10 @@ int main(int ac, char**av) {
 
   while (42) {
     ansigraphic_imagePrint(img);
-    usleep(10000);
+    usleep(1000000);
     ansigraphic_imagePrint_RGB(imgrgb);
     ansigraphic_image_RGB_reverse(imgrgb);
-    usleep(10000);
+    usleep(1000000);
   }
   ansigraphic_deleteImage(img);
   ansigraphic_deleteImage_RGB(imgrgb);
